b5001d039835382a212156f8586ec11f68b56ee3
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-06-06 00:16:57 +0900

    架空サイト（石井花壇）　※デザインは他者

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  speed: 4000,
+  loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  
+});
